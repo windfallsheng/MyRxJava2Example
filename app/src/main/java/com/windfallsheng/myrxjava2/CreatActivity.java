33abeec8ff968c1a6938d5b459baa14613a05efc
WindfallSheng <windfallsheng@aliyun.com>
2019-06-30 23:18:48 +0800

    no message

--- app/src/main/java/com/windfallsheng/myrxjava2/CreatActivity.java
+++ app/src/main/java/com/windfallsheng/myrxjava2/CreatActivity.java
@@ -1,30 +1,29 @@
 package com.windfallsheng.myrxjava2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.TextView;
-
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
-public class MainActivity extends AppCompatActivity {
+public class CreatActivity extends AppCompatActivity {
 
-    private final String TAG = MainActivity.class.getSimpleName();
-//    @BindView(R.id.textview1)
-//    public TextView tv1;
-//    @BindView(R.id.textview2)
-//    public TextureView tv2;
+    private final String TAG = CreatActivity.class.getSimpleName();
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, CreatActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
-
+        setContentView(R.layout.activity_creat);
         Observable.concat(
                 Observable.just(1, 2, 3),
                 Observable.just(4, 5, 6),
@@ -51,18 +50,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, "-->onComplete()_对Complete事件作出响应\n");
                     }
                 });
-
     }
 
-//    @OnClick({R.id.textview1, R.id.textview2})
-//    public void onViewClicked(View view) {
-//        switch (view.getId()) {
-//            case R.id.textview1:
-//                Log.d(TAG, "我是点击事件1");
-//                break;
-//            case R.id.textview2:
-//                Log.d(TAG, "我是点击事件2");
-//                break;
-//        }
-//    }
+
 }
